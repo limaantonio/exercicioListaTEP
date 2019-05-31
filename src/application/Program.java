99dbd24665f7f301c5cd81f06dd6b2fe34996b48
antonio <limaantoniocarlos20@gmail.com>
2019-05-31 13:07:56 -0300

    Esse é o exercicio da aula 12 de TEP sobre listas

---
+++ src/application/Program.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		double s, tx;
+		String name;
+		int id, n, ident;
+		ArrayList<Funcionario> list = new ArrayList();
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("Employer #%d:\n",i+1);
+			System.out.println("Id: ");
+			id = sc.nextInt();
+			System.out.println("Name: ");
+			sc.nextLine();
+			name = sc.nextLine();
+			System.out.println("Salary: ");
+			s = sc.nextDouble();
+			
+			Funcionario f = new Funcionario(id, name, s);
+			
+			list.add(f);	
+		}
+		System.out.println("Enter the employee id that will have salary increase:");
+		ident = sc.nextInt();
+		
+		for(Funcionario f : list) {
+			if(f.getId() == ident) {
+				System.out.println("Enter the percentage:");
+				tx = sc.nextDouble();	
+				f.aumentaSalario(tx);
+			}else {
+				System.out.println("This employee does not exist.");
+				System.exit(0);
+			}
+		}
+		System.out.println("List of employee");
+		for(Funcionario f : list) {
+			System.out.println(f.getId()+", "+f.getNome()+", "+f.getSalario());
+		}
+	}
+
+}
